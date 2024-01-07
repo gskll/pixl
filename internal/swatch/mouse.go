af840fca0a885a1e7b9fe36a5767761591bf6a49
sp-andrew-gaskell <andrew.gaskell@socialpoint.es>
2024-01-07 12:19:20 +0100

    implement fyne swatch functionality/interfaces

---
+++ internal/swatch/mouse.go
@@ -0,0 +1,12 @@
+package swatch
+
+import (
+	"fyne.io/fyne/driver/desktop"
+)
+
+func (s *Swatch) MouseDown(ev *desktop.MouseEvent) {
+	s.clickHandler(s)
+	s.Selected = true
+	s.Refresh()
+}
+func (s *Swatch) MouseUp(ev *desktop.MouseEvent) {}
